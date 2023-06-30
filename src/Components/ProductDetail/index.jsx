eec54a515eec132a15f96733b6811ca485086750
Sebastián Debesa <sebastian.debesa@hotmail.com>
2023-06-29 22:44:19 -0300

    Item detail buttons funcionality implemented

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,16 +1,26 @@
-import { XMarkIcon } from '@heroicons/react/24/solid';
+import { useContext } from "react";
+import { XMarkIcon } from "@heroicons/react/24/solid";
+import { ShoppingCartContext } from "../../Context";
 
 const ProductDetail = () => {
-    return(
-        <aside className="flex flex-col fixed right-0 border border-black rounded-lg bg-white w-[360px] h-[calc(100vh-80px)]">
-            <div className='flex justify-between items-center py-2 px-4'>
-                <h2 className='font-medium text-xl'>Detail</h2>
-                <div> <XMarkIcon className='h-6 w-6 text-black-600'/> </div>
-
-            </div>
-
-        </aside>
-    )
-}
+  const context = useContext(ShoppingCartContext);
+  return (
+    <aside
+      className={`${
+        context.isItemDetailOpen ? "flex" : "hidden"
+      } flex flex-col fixed right-0 border border-black rounded-lg bg-white w-[360px] h-[calc(100vh-80px)]`}
+    >
+      <div className="flex justify-between items-center py-2 px-4">
+        <h2 className="font-medium text-xl">Detail</h2>
+        <div>
+          <XMarkIcon
+            className="h-6 w-6 text-black-600"
+            onClick={() => context.closeItemDetail()}
+          />
+        </div>
+      </div>
+    </aside>
+  );
+};
 
 export default ProductDetail;
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -6,8 +6,24 @@ export const ShoppingCartContext = createContext();
 // eslint-disable-next-line react/prop-types
 export const ShoppingCartProvider = ({ children }) => {
   const [count, setCount] = useState(0);
+  const [isItemDetailOpen, setItemDetailOpen] = useState(false);
+  const openItemDetail = () => {
+    setItemDetailOpen(true);
+  };
+  const closeItemDetail = () => {
+    setItemDetailOpen(false);
+  };
+
   return (
-    <ShoppingCartContext.Provider value={{ count, setCount }}>
+    <ShoppingCartContext.Provider
+      value={{
+        count,
+        setCount,
+        isItemDetailOpen,
+        openItemDetail,
+        closeItemDetail,
+      }}
+    >
       {children}
     </ShoppingCartContext.Provider>
   );
